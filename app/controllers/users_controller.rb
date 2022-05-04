bb404bce56ba967a7a991357b97c960f058f3a5b
masafumikazama <masanomaildesu@yahoo.co.jp>
2022-05-04 12:23:04 +1000

    [Finish]4-17

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def show
+    @user = current_user
+    @post_images = @user.post_images
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to user_path
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :post_images, only: [:new, :index, :show, :create, :destroy]
+  resources :users, only: [:show, :edit, :update]
 
   get '/homes/about' => "homes#about", as: :about
 end
